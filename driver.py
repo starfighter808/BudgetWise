6ea5675e7345a8cffb57b7a8e3034f29de46f983
Antony-Po <144125457+Antony-Po@users.noreply.github.com>
2025-01-31 15:03:31 -0600

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,8 +1,8 @@
 from database import Database
 from user import User
 
-db = Database()
-user_manager = User(db)
+conn = Database()
+user_manager = User(conn)
 user_manager.create_user("Joe", "apassword123")
 print(user_manager.get_user_id("Joe", "apassword123"))
-db.close()
+conn.close()
