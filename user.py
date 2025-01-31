a25a0842f251626621c9d7a0c6ebe1b794340ffa
Antony-Po <144125457+Antony-Po@users.noreply.github.com>
2025-01-31 15:14:13 -0600

    Update user.py

--- user.py
+++ user.py
@@ -5,15 +5,15 @@
 from database import Database
 
 class User:
-    def __init__(self, db: Database):
-        self.db = db
+    def __init__(self, conn: Database):
+        self.conn = conn
 
     def create_user(self, username, password_hash):
         query = "INSERT INTO users (username, passwordhash, theme, notificationsEnabled, Language, default_chart) VALUES (?, ?, ?, ?, ?, ?)"
-        self.db.execute(query, (username, password_hash, 0, 1, 0, 0))
+        self.conn.execute(query, (username, password_hash, 0, 1, 0, 0))
 
     def get_user_id(self, username, password_hash):
         query = "SELECT * FROM users WHERE username = ? AND passwordhash = ?"
-        return self.db.fetchone(query, (username, password_hash))
+        return self.conn.fetchone(query, (username, password_hash))
     
 
