bc596a151a4587537c92610cc3da559e86781e7b
Justin P. <158069555+Krackerbarrel@users.noreply.github.com>
2025-02-17 12:26:11 -0600

    Updated database to meet current er diagram
    Just updated to current

--- Database.py
+++ Database.py
@@ -40,7 +40,10 @@ class database():
                     Language INTEGER,
                     default_chart INTEGER,
                     createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-                    updatedAT DATETIME DEFAULT CURRENT_TIMESTAMP
+                    updatedAT DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    weekly_reports INTEGER,
+                    monthly_reports INTEGER,
+                    yearly_reports INTEGER
                 )''')
 
         # Create security_questions table
@@ -67,8 +70,6 @@ class database():
                     end_Date DATETIME
                 )''')
 
-        # Create category table with reference to users table
-
         #   Create budget_accounts table with references to users, category, and budget tables
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS budget_accounts (
                     budget_accounts_id INTEGER PRIMARY KEY,
@@ -88,9 +89,7 @@ class database():
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS vendor (
                     vendor_id INTEGER PRIMARY KEY,
                     the_user INTEGER REFERENCES users(userID),
-                    vendor_name TEXT,
-                    description TEXT,
-                    vendor_usage_ranking INTEGER
+                    vendor_name TEXT
                 )''')
 
         # Create alerts table with references to budget_accounts and users tables
@@ -114,8 +113,7 @@ class database():
                     the_user INTEGER REFERENCES users(userID),
                     report_type INTEGER,
                     report_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-                    report_data BLOB,
-                    report_frequency INTEGER
+                    report_data BLOB
                 )''')
    
         # Create transactions table with references to budget_accounts, users, category, and vendor tables
