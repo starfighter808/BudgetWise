deb616dcc405a5e13446bfccc6561d8d92ebfea9
rragusa12 <144153843+rragusa12@users.noreply.github.com>
2025-02-19 14:34:32 -0600

    Update login.py
    updated how this class called the database and cursor, along with how it interacts with user.py

--- budgetwise test combination/login.py
+++ budgetwise test combination/login.py
@@ -1,55 +1,43 @@
 from argon2 import PasswordHasher, exceptions
-from Database import database
 from user import User
+from Database import database
 
 class Login:
     def __init__(self):
         self.ph = PasswordHasher()
-        self.db = database("BudgetWise")  
-        self.user_manager = User(self.db)
+        self.db = database()  
+        self.user_manager = User() 
 
-    def verify_password(self,stored_hash, provided_password):
+    def verify_password(self, stored_hash, provided_password):
         try:
-            # This will raise an exception if the password doesn't match.
             self.ph.verify(stored_hash, provided_password)
             return True
         except exceptions.VerifyMismatchError:
             return False
 
     def login(self, user, pwd) -> str:
-        username_password_tuple = self.pullUser(user)
-
-        if username_password_tuple is None:
-            print("user id is empty")
+        user_record = self.pullUser(user)
+        if user_record is None:
+            print("User not found.")
             return None
 
-        stored_user_username = username_password_tuple[0]
-        stored_user_password = username_password_tuple[1]
-
-        if self.verify_password(stored_user_password, pwd):
-            print("passsed the verify check")
-            return stored_user_username
+        stored_username, stored_password = user_record
+        if self.verify_password(stored_password, pwd):
+            print("Login successful.")
+            return stored_username
         else:
-            print("failed the verify check")
+            print("Invalid password.")
             return None
         
     def signUp(self, user: str, pwd: str):
-        # takeInput()
-        # isUnique()
-        hash = self.hashInput(pwd)
-        self.addUser(user, hash)
+        hashed_pwd = self.hashInput(pwd)
+        self.addUser(user, hashed_pwd)
     
-    # hashes and returns password
     def hashInput(self, pwd: str) -> str:
         return self.ph.hash(pwd)
     
-    # creates User object and adds new user to database
-    def addUser(self, user: str, hash: str):
-        self.user_manager.create_user(user, hash)
+    def addUser(self, user: str, hashed_pwd: str):
+        self.user_manager.create_user(user, hashed_pwd)
 
     def pullUser(self, stored_username: str):
-        userID = self.user_manager.get_user_id(stored_username)
-        return userID
-
-
-#
+        return self.user_manager.get_user_id(stored_username)
