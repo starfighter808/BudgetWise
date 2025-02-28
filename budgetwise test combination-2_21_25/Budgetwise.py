c08f0bce37c5f2219a58098bff8457343b840290
Justin P. <158069555+Krackerbarrel@users.noreply.github.com>
2025-02-28 11:44:55 -0600

    Well then Imports change didn't send

--- budgetwise test combination-2_21_25/Budgetwise.py
+++ budgetwise test combination-2_21_25/Budgetwise.py
@@ -1,18 +1,4 @@
-import flet as ft
-import atexit
-from Database import Database
-from BWScenes import WelcomeScene
-from BWDash import DashboardScene
-from BWAccounts import AccountScene
-from BWTransactions import TransactionScene
-from BWForms import LoginScene, SignInScene, SecurityQuestionsForm, AccountCreationForm, BudgetCreationForm
-from DataManager import DataManager
-from installation import Installation
-from BWHistory import HistoryScene
-from BWMenu import MenuBar    
-import os
-from user import User
-from pathlib import Path
+from BWImports import *
         
 def main(page: ft.Page):
 
