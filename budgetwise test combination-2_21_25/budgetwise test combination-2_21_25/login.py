9ce9f2ca52724bdf9a816bb037afa92a3ffa8092
rragusa12 <144153843+rragusa12@users.noreply.github.com>
2025-02-28 10:36:53 -0600

    Add files via upload

---
+++ budgetwise test combination-2_21_25/budgetwise test combination-2_21_25/login.py
@@ -0,0 +1,43 @@
+from argon2 import PasswordHasher, exceptions
+from user import User
+from Database import database
+
+class Login:
+    def __init__(self):
+        self.ph = PasswordHasher()
+        self.db = database()  
+        self.user_manager = User() 
+
+    def verify_password(self, stored_hash, provided_password):
+        try:
+            self.ph.verify(stored_hash, provided_password)
+            return True
+        except exceptions.VerifyMismatchError:
+            return False
+
+    def login(self, user, pwd) -> str:
+        user_record = self.pullUser(user)
+        if user_record is None:
+            print("User not found.")
+            return None
+
+        stored_username, stored_password = user_record
+        if self.verify_password(stored_password, pwd):
+            print("Login successful.")
+            return stored_username
+        else:
+            print("Invalid password.")
+            return None
+        
+    def signUp(self, user: str, pwd: str):
+        hashed_pwd = self.hashInput(pwd)
+        self.addUser(user, hashed_pwd)
+    
+    def hashInput(self, pwd: str) -> str:
+        return self.ph.hash(pwd)
+    
+    def addUser(self, user: str, hashed_pwd: str):
+        self.user_manager.create_user(user, hashed_pwd)
+
+    def pullUser(self, stored_username: str):
+        return self.user_manager.get_user_id(stored_username)
