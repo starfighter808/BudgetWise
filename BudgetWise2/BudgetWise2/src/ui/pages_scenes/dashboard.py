ede3f43e812c3d241df5c72af7e9faa19b37274b
rragusa12 <144153843+rragusa12@users.noreply.github.com>
2025-03-17 09:13:51 -0500

    Delete BudgetWise2 directory

--- BudgetWise2/BudgetWise2/src/ui/pages_scenes/dashboard.py
+++
@@ -1,8 +0,0 @@
-import flet as ft
-
-class Dashboard(ft.View):
-    def __init__(self, page: ft.Page, user_repo):
-        super().__init__(route="/dashboard", bgcolor="#5C9DFF")
-    
-        self.controls.append(ft.Text("Dashbaord"))
-    
